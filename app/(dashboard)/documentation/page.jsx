232789c05b09e64396d624b519d293189b646bd5
青荼 <10788202+color-tea@user.noreply.gitee.com>
2023-08-02 15:57:13 +0800

    fix documents

--- app/(dashboard)/documentation/page.jsx
+++ app/(dashboard)/documentation/page.jsx
@@ -17,15 +17,14 @@ export default async function Page() {
       </LargeHeading>
       <div className="mt-2 w-full">
         <Paragraph className="max-w-full text-left ">
-          该软件设计用于针对数据关联渲染，可以通过此页面分析数据之前的关系.
           目前支持的功能如下：
         </Paragraph>
       </div>
       <ul>
-        <li>1. Paint</li>
-        <li>2. User</li>
-        <li>2. Data</li>
-        <li>2. Releases</li>
+        <li>1. Paint TODO </li>
+        <li>2. User TODO </li>
+        <li>2. Data TODO </li>
+        <li>2. Releases TODO </li>
       </ul>
     </div>
   );
